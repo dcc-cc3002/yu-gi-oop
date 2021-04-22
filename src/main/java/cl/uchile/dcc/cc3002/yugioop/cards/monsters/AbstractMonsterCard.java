39a40ff5a66d37044b91e7931e6d2ca89164041c
SL8R <islaterm@gmail.com>
2021-04-22 18:40:49 -0400

    feat(cards): Extra and main deck monster cards

--- src/main/java/cl/uchile/dcc/cc3002/yugioop/cards/monsters/AbstractMonsterCard.java
+++ src/main/java/cl/uchile/dcc/cc3002/yugioop/cards/monsters/AbstractMonsterCard.java
@@ -3,17 +3,19 @@
 // Creative Commons Attribution 4.0 International License.
 // You should have received a copy of the license along with this
 // work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
-package cl.uchile.dcc.cc3002.yugioop.cards;
+package cl.uchile.dcc.cc3002.yugioop.cards.monsters;
+
+import cl.uchile.dcc.cc3002.yugioop.cards.Card;
 
 /**
  * @author <a href=mailto:ignacio.slater@ug.uchile.cl>Ignacio Slater Muñoz</a>
  */
-public class MonsterCard extends Card {
+public abstract class AbstractMonsterCard extends Card {
   private int level;
   private int attack;
   private int defense;
 
-  public MonsterCard(String name, int level, int attack, int defense) {
+  protected AbstractMonsterCard(String name, int level, int attack, int defense) {
     super(name);
     this.level = level;
     this.attack = attack;
