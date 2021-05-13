124a304d7ce67f57ee4778f8144cb5467fea4ac5
SL8R <islaterm@gmail.com>
2021-05-13 19:19:46 -0400

    feat(game-mat): Monster zone and backrow creation

--- src/main/java/cl/uchile/dcc/cc3002/yugioop/GameMat.java
+++ src/main/java/cl/uchile/dcc/cc3002/yugioop/GameMat.java
@@ -7,8 +7,50 @@
  */
 package cl.uchile.dcc.cc3002.yugioop;
 
+import cl.uchile.dcc.cc3002.yugioop.cards.BackrowCard;
+import cl.uchile.dcc.cc3002.yugioop.cards.monsters.MonsterCard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * @author <a href=mailto:ignacio.slater@ug.uchile.cl>Ignacio Slater Muñoz</a>
  */
 public class GameMat {
+  private final Player player;
+  private final List<MonsterCard> monsters = new ArrayList<>();
+  private final List<BackrowCard> backrow = new ArrayList<>();
+
+  public GameMat(Player player) {
+    this.player = player;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(GameMat.class, player);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof GameMat) {
+      var other = (GameMat) obj;
+      return player.equals(other.player);
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return "PlayerMat{\n\t" + player.toString() + ",\n\t" + monsters.toString() + ",\n\t"
+           + backrow.toString() + "\n}";
+  }
+
+  public List<MonsterCard> getMonsters() {
+    return List.copyOf(monsters);
+  }
+
+  public List<BackrowCard> getBackrow() {
+    return List.copyOf(backrow);
+  }
 }
--- src/test/java/cl/uchile/dcc/cc3002/yugioop/PlayerMatTest.java
+++ src/test/java/cl/uchile/dcc/cc3002/yugioop/PlayerMatTest.java
@@ -10,23 +10,37 @@ package cl.uchile.dcc.cc3002.yugioop;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author <a href=mailto:ignacio.slater@ug.uchile.cl>Ignacio Slater Muñoz</a>
  */
 public class PlayerMatTest {
+  private final static String YUGI_NAME = "Yugi";
+  private final static String JUDAI_NAME = "Judai";
   private GameMat mat;
+  private Player yugi;
+  private Player judai;
 
   @BeforeEach
   void setUp() {
-    mat = new GameMat();
+    yugi = new Player(YUGI_NAME);
+    judai = new Player(JUDAI_NAME);
+    mat = new GameMat(yugi);
   }
 
   @Test
   void constructorTest() {
-    var expectedMat = new GameMat();
+    var expectedMat = new GameMat(yugi);
     assertEquals(expectedMat, mat);
     assertEquals(expectedMat.hashCode(), mat.hashCode(), "Hashes don't match");
+    var differentPlayerMat = new GameMat(judai);
+    assertNotEquals(differentPlayerMat, mat,
+                    mat.toString() + " should be different to " + differentPlayerMat.toString());
+
+    assertTrue(mat.getMonsters().isEmpty(),
+               "Monster zone should be empty on creation, but was " + mat.getMonsters().toString());
+    assertTrue(mat.getBackrow().isEmpty(),
+               "Backrow should be empty on creation, but was " + mat.getBackrow().toString());
   }
 }
